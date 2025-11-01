6efe168f91390c3796bc5a8422830cbc76080221
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2025-11-01 09:30:10 +0545

    Made changes in payment methods

--- src/components/TransactionsElements.jsx
+++ src/components/TransactionsElements.jsx
@@ -12,23 +12,23 @@ const TransactionsElements = ({
   return (
     <section>
       <div
-        className={`flex border rounded-2xl px-5 py-4  items-center gap-2 justify-between`}
+        className={`flex border rounded-2xl px-5 py-4  items-center gap-1 sm:gap-2 justify-between`}
       >
         <div className={`icon px-3 py-3 bg-gray-300 rounded-3xl `}>
           <Icon size={20} />
         </div>
         <div className="textLeft">
           <p className="font-sans text-md sm:text-lg">{label}</p>
-          <p className="font-semibold text-gray-600">{subLabel}</p>
+          <p className="font-semibold text-sm sm:text-lg text-gray-600">
+            {subLabel}
+          </p>
         </div>
         <div className={`state px-3 py-1 ${stateBg} text-sm rounded-3xl`}>
           <p className="font-sans ">{state}</p>
         </div>
         <div className="textRight">
           <p className="font-sans text-md sm:text-xl ">{balance}</p>
-          <p className={`text-xs`} >
-            {bonus}
-          </p>
+          <p className={`text-xs`}>{bonus}</p>
         </div>
       </div>
     </section>
